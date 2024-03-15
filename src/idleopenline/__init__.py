754a790d7a23233dde968548811a4359d915a27e
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2024-03-15 02:07:09 -0500

    Fix metadata issues Fix description and remove Framework:Trio tag.

--- src/idleopenline/__init__.py
+++ src/idleopenline/__init__.py
@@ -22,7 +22,7 @@ from __future__ import annotations
 __title__ = "idleopenline"
 __author__ = "CoolCat467"
 __license__ = "GNU General Public License Version 3"
-__version__ = "0.0.0"
+__version__ = "0.0.1"
 
 
 from idleopenline import utils
