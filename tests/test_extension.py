9d9b908bed0ac7ce214746a1aa9ea14e91b3f4d2
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2025-06-21 16:58:23 -0500

    Test windows `FilePosition` parsing

--- tests/test_extension.py
+++ tests/test_extension.py
@@ -1,7 +1,18 @@
+import sys
+from typing import Final
+
+import pytest
+
 from idleopenline import extension
 
+IS_WINDOWS: Final = sys.platform == "win32"
 
-def test_fileposition_parse() -> None:
+
+@pytest.mark.skipif(
+    IS_WINDOWS,
+    reason="Skipping Unix-specific tests on Windows",
+)
+def test_fileposition_parse_unix() -> None:
     assert extension.FilePosition.parse(
         "src/idleopenline/extension.py:59",
     ) == extension.FilePosition("src/idleopenline/extension.py", 59, 0, 59, 0)
@@ -36,6 +47,52 @@ def test_fileposition_parse() -> None:
         "src/idleopenline/extension.py:59:43:60:48:103",
     ) == extension.FilePosition(
         "src/idleopenline/extension.py",
+        59,
+        43,
+        60,
+        48,
+    )
+
+
+@pytest.mark.skipif(
+    not IS_WINDOWS,
+    reason="Skipping Windows-specific tests on non-Windows platforms",
+)
+def test_fileposition_parse_windows() -> None:
+    assert extension.FilePosition.parse(
+        "C:\\path\\to\\file.py:59",
+    ) == extension.FilePosition("C:\\path\\to\\file.py", 59, 0, 59, 0)
+    assert extension.FilePosition.parse(
+        "C:\\path\\to\\file.py:59:43",
+    ) == extension.FilePosition(
+        "C:\\path\\to\\file.py",
+        59,
+        43,
+        59,
+        43,
+    )
+    assert extension.FilePosition.parse(
+        "C:\\path\\to\\file.py:59:43:60",
+    ) == extension.FilePosition(
+        "C:\\path\\to\\file.py",
+        59,
+        43,
+        59,
+        43,
+    )
+    assert extension.FilePosition.parse(
+        "C:\\path\\to\\file.py:59:43:60:48",
+    ) == extension.FilePosition(
+        "C:\\path\\to\\file.py",
+        59,
+        43,
+        60,
+        48,
+    )
+    assert extension.FilePosition.parse(
+        "C:\\path\\to\\file.py:59:43:60:48:103",
+    ) == extension.FilePosition(
+        "C:\\path\\to\\file.py",
         59,
         43,
         60,
