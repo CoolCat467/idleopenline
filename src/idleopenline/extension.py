3c841412dac7d3590f9427defbbe2439d947557c
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2025-06-21 16:51:41 -0500

    Bump to v0.0.2

--- src/idleopenline/extension.py
+++ src/idleopenline/extension.py
@@ -5,7 +5,7 @@
 from __future__ import annotations
 
 # IDLE Open Line Extension
-# Copyright (C) 2024  CoolCat467
+# Copyright (C) 2024-2025  CoolCat467
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
